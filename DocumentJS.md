89f363992dd6b65eda3eaab4b45c803864e17fbf
mangtrungzos <sangv906@gmail.com>
2022-12-09 19:46:19 +0700

    12/9

--- DocumentJS.md
+++ DocumentJS.md
@@ -5,7 +5,7 @@
 1. alert : tại 1 thời điểm thì chỉ hiển thị 1 biến mà thôi
 
 # Một số hàm built-in (built-in function)
-    1. Alert
+    1. Alert : Tạo ra 1 bảng  thông báo
     2. Console : đối tượng phương thức 
     3. Confirm : Tạo ra 1 box để xác nhận
     4. Prompt : kết hợp cả alert và confirm và có  thêm ô input
@@ -21,6 +21,40 @@
 - Comments nhiều dòng
 ## Sử dụng phím tắt
 - Windows: ctrl + /
+
+# Kiểu dữ liệu trong Javascript
+// Gồm có 6 kiểu:
+- 0
+- false
+- '' - ""
+- undefined
+- NaN
+- null
+=> 6 kiểu giá trị này khi convert sang boolean sẽ là false còn khác là true
+> 1. Kiểu dữ liệu nguyên thủy - Primitive Data
+    - Number 
+    - String : Được thể hiện bằng cặp '..' or ".."
+    - Boolean
+    - Undefined : Là 1 kiểu chỉ khai báo biến nhưng không gán bất cứ giá trị nào
+    - Null
+    - Symbol : Có 1 tham số truyền vào là description 
+>> Notes: Cách hiện dấu nháy trong 1 cặp dấu nháy 
+    EX: var fullName = 'Sang \'vu'
+        console.log(fullName);  
+
+    EX: 1 Trường hợp khác sẽ không bị lỗi (do hai dấu khác nhau nên sẽ không sinh ra lỗi)
+        var fullName = "sang 'vu";
+        console.log(fullName);
+
+
+> 2. Kiểu dữ liệu phức tạp - Complex Data
+    - Function
+    - Object
+
+>> Notes: 
+>>> Object Sử dụng để lưu hầu hết các giá trị trong Javascript
+>>> Object sử dụng để định nghĩa key
+>>> Khi cần định nghĩa 1 danh sách thì dung Array (Array cũng là 1 phần thuộc Object) 
 
 # Toán tử
 1. Toán tử số học - Arithmetic
@@ -38,5 +72,77 @@
  ++ -> tăng 1 giá trị số
  -- -> giảm 1 giá trị số
 
+## Toán Tử gán
+ Toán tử       Ví dụ          Tương đương
+ =             x = y          x = y
+ +=            x += y         x = x + y
+ -=            x -= y         x = x - y
+ *=            x *= y         x = x * y
+ /=            x /= y         x = x / y
+ **=           x **= y        x = x ** y
+
+>> x++ tăng giá trị biến lên 1 và trả về giá trị trước khi tăng
+>> ++x tăng giá trị biến lên 1 và trả về giá trị sau khi tăng
+>> x-- giảm giá trị biến xuống 1 và trả về giá trị trước khi giảm
+>> --x giảm giá trị biến xuống 1 và trả về giá trị sau khi giảmm
+
+## Toán Tử ++ --
+### Prefix & Postfix (Tiền tố và hậu tố)
+
+## Toán tử chuỗi - String operator
+
+## Toán tử so sánh
+== : Bằng 
+!= : không bằng
+> : Lớn hơn
+<: Bé hơn
+>= : Lớn hơn hoặc bằng
+<= : Nhỏ hơn hoặc bằng
+
+> Luôn trả về 2 kết quả đúng hoặc sai
+> =, != : luôn chỉ quan tâm tới value của biến
+--------------------------------------
+## Toán tử so sánh - P2
+=== : bằng
+!== : không bằng
+
+> Notes: === so sánh cả data type, cả kiểu dữ liệu (so sánh tuyệt đối)
+> Note: !== so sánh tuyệt đối 
 
 
+## Boolean
+- two value : true or false
+### Truthy & Falsy
+> Truthy - to bool is true
+- !! trong boolean. Toán tử ! là toán tử not(phủ định) nên !! là 2 lần phủ định sẽ trở thành "khẳng định"
+
+- Thêm !! phía trước các giá trị truthy sẽ luôn trả về true
+
+> Falsy - to bool is false
+1. false
+2. 0
+3. '' or "" (chuỗi rỗng)
+4. null
+5. undefined
+6. NaN
+
+>> Notes: Bất cứ giá trị nào trong Javascript khi chuyển đổi sang kiểu dữ liệu boolean mà có giá trị false thì ta gọi giá trị đó là Falsy.
+
+>> Notes: 6 giá trị đề cập tới Falsy thì all value khác đều là true. Kể cả giá trị sau:
+1. '0' (Một chuỗi chứa số không)
+2. ' ' (Một chuỗi chứa dấu cách)
+3. 'false' (một chuỗi chứa từ khóa false)
+4. [] (Một arry trống)
+5. {} (một object trống)
+6. function(){} (một hàm "trống)
+
+
+## Toán tử logic
+1. && - And
+2. || - Or
+3. ! - Not
+
+> Note: 
+>> Với toán tử hoặc (||), nếu xuất hiện ít nhất 1 vế bằng true thì kết quả trả về sẽ là true.
+
+>> Còn với toán tử và (&&), nếu xuất hiện ít nhất 1 vế bằng false thì kết quả trả về sẽ là false.
--- main.js
+++ main.js
@@ -42,19 +42,19 @@ setInterval(function() {
 }, 1000)
 */
 
-// Toán tử số học
+        // Toán tử số học
 /*
 var a = 1 + 2;
 console.log(a)
 */ 
 
-// Toán tử gán = 
+        // Toán tử gán = 
 /*
 var fullName = ' vu trong sang'; / Gán giá trị vu trong sang cho biến fullName 
 console.log(fullName)
 */ 
 
-// Toán tử so sánh
+        // Toán tử so sánh
 /* var a = 1;
  var b = 2;
  if(a < b){
@@ -62,7 +62,7 @@ console.log(fullName)
  }
 */
 
-// Toán tử logic
+        // Toán tử logic
 /*
 var a = 1;
 var b = 2;
@@ -71,12 +71,253 @@ if(a > 0 && b > 0){
 }
 */
 
-// Practice Arithmetic
+        // Practice Arithmetic
 /* 
 var a = 2;
 a++;
 
 console.log(a)
 */
+
+        // Practice Assignment
+// var a = 1;
+
+// a /= 2;
+// console.log(a);
+
+
+
+        // Toán tử ++ -- / Prefix & Postfix
+// var a = 6;
+
+// Việc 1: + 1 cho a, a = a + 1 => a =7
+// Việc 2: Trả về a sau khi được + 1
+
+
+// Việc 1: 'a copy', 'a copy' = 6
+// Việc 2: + 1 cho a, a = a + 1 => a = 7
+// Việc 3: trả về ' a copy' a = 6
+// var output = a++;
+// console.log('output:', output);
+// console.log('a:', a);
+
+// var number = 6;
+
+// var output = number++ + --number;
+
+// 6 + 6 
+// number++ trả về 6 sau khi tính xong thì number đã tăng lên 1 nên biến number ở TH: --number là 7 và giảm đi 1 tại thời điểm đó
+
+// console.log('output', output);
+
+// var output = ++number * 2 - number-- * 2;
+
+// // 7 * 2 - 7 * 2
+// // number lúc đầu là 7, number-- trả về biến copy => = 7 
+
+// console.log('output:', output);
+
+
+
+                // Toán tử chuỗi - String operator
+// var firstName = 'sang'
+// var lastName = 'vu'
+
+
+// console.log(firstName + ' ' + lastName);
+
+
+// var name = 'sang'
+
+// name = name + ' vu';
+
+// console.log(name);
+
+
+
+                // Toán tử so sánh
+// var a = 1;
+// var b = 2;
+
+// if (a != b) {
+//         console.log('Dieu kien dung!');
+// } else{
+//         console.log('Dieu kien sai!');
+// }
+
+
+// var a = 'vu trong sang';
+// var b = 'vu trong sang';
+
+// if (a == b) {
+//         console.log('Dieu kien dung!');
+// } else{
+//         console.log('Dieu kien sai!');
+// }
+
+
+                // Boolean
+// var a = 1;
+// var b = 2;
+
+// var isSuccess = a < b;
+
+// console.log(isSuccess);
+
+
+/**
+ * If - else
+ */
+
+/**
+ * 0
+ * false
+ * '' - ""
+ * undefined
+ * NaN
+ * null
+ */
+// var fullName = '0';
+
+// if (fullName){
+//         console.log('Dieu kien dung!');
+// }  else{
+//         console.log('Dieu kien sai!');
+// }
+
+
+                // Toán tử logic
+// var a = 1;
+// var b = 2;
+// var c = 3;
+
+// if(a > 0 && b < 0 && c > 0){
+//         console.log('Dieu kien dung!');
+// } else{
+//         console.log('Dieu kien sai!');
+// }
+
+                // Data in Javascript
+// Number type
+var a = 1;
+var b = 2;
+var c = 1.5;
+
+/// Use keyword 'Typeof' to check
+console.log(typeof a)
+
+// String type
+var fullName = 'sang vu';
+
+/// Use keyword 'Typeof' to check
+console.log(typeof fullName)
+
+// Boolean 
+var isSuccess = true;  false
+
+console.log(typeof isSuccess)
+
+// Undefined type
+var age;
+
+console.log(typeof age)
+
+// Null
+var isNull = null; // nothing
+
+console.log(typeof isNull)
+
+// Symbol
+var id = Symbol('id'); // Đặc tính của Symbol => unique : Tính duy nhất
+var id2 = Symbol('id');
+// Nên id khác id2
+
+console.log(typeof Symbol)
+
+// Function
+
+var myFunction = function(){
+        // alert('hi everyone!');
+}
+myFunction();
+
+// Object types
+
+var myObject = {
+        name: 'Sang vu',
+        age: 20,
+        address: 'Vung Tau',
+        myFunction: function(){
+
+        }
+};
+// console.log('myObject', myObject);
+console.log(typeof myObject)
+
+
+var myArray = [
+        'Javascript',
+        'PHP',
+        'Python'
+];
+
+// console.log(myArray)
+console.log(typeof myArray)
+
+
+// Ex Data in Javascript
+var a = '1';
+var b = 2;
+
+var c = typeof a;
+var d = typeof b;
+var e = typeof d;
+
+console.log(c, d, e) // Output: ?
+
+/**
+ * typeof của 1 số sẽ trả về 'Number'
+ * typeof của 1 chuỗi sẽ trả về 'string'
+ * Note: Kết quả trả về của typeof sẽ luôn là 1 chuỗi, vậy nên typeof của d sẽ là 'string'
+ */
+
+        
+
+                // Toán tử so sánh - P2
+/**
+ * === : so sánh tuyệt đối
+ * !== : so sánh tuyệt đối
+ */
+var a = 1;
+var b = '1';
+
+console.log(a !== b);
+
+
+                // Truthy / Falsy - to bool is true / false
+
+/// EX Truthy - to bool is true
+/// Các giá trị dưới đây khi convert sang boolean sẽ nhận giá trị là 'true'
+console.log(Boolean(1)) // true
+console.log(Boolean(['BMW'])) // true
+console.log(Boolean({ name: 'Miu' })) // true
+
+console.log(!!'hi') // true
+
+console.log(!!1) // true
+console.log(!!'f8') // true
+console.log(!!['Mercedes']) // true
+
+// Thêm '!!' phía trước giá trị truthy luôn trả về true
+
+
+/// Ex Falsy - to bool is false
+console.log(!!false) // false
+console.log(!!0) // false
+console.log(!!'') // false
+console.log(!!null) // false
+console.log(!!undefined) // false
+console.log(!!NaN) // false
+
 
 
